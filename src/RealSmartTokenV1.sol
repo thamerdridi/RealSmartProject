6bed8859b87a236bea79979cf36e16f0cf20020d
Thamer Dridi <thamer.dridi@etudiant-isi.utm.tn>
2025-09-30 00:46:02 +0100

    Added the 1st version of RealSmart Token COntract

---
+++ src/RealSmartTokenV1.sol
@@ -0,0 +1,35 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20PausableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+
+contract RealSmartTokenV1 is OwnableUpgradeable, UUPSUpgradeable, ERC20PausableUpgradeable {
+/// @custom:oz-upgrades-unsafe-allow constructor
+    constructor() { _disableInitializers(); }
+
+
+    function initialize(string memory _name, string memory _symbol) initializer public {
+        __Ownable_init(msg.sender);
+        __UUPSUpgradeable_init();
+        __ERC20_init(_name, _symbol);   
+        __Pausable_init();
+    }
+
+    function mint(address to, uint256 amount) external onlyOwner {
+        _mint(to, amount);
+    }
+    function burn(address from, uint256 amount) external onlyOwner {
+        _burn(from, amount);
+    }
+    function pause() external onlyOwner {
+        _pause();
+    }
+    function unpause() external onlyOwner {
+        _unpause();
+    }
+
+
+    function _authorizeUpgrade(address newImplementation) internal onlyOwner override {}
+
+}
